7de29e05b49860ea682e5d1f2b2fe37f72f55c0e
Kashaedov Vitaliy <Vitalickashaedov@mail.ru>
2022-07-14 19:45:58 +0300

    Zadacha38

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿int size = 5; 
+double [] array = new double [size];
+
+
+void FillArray (double [] array)
+
+{
+    Random random = new Random ();
+    for (int i = 0; i < array.Length; i++)
+    {
+    array [i] = random.NextDouble ();
+    }
+}
+
+void PrintArray (double [] array)
+{
+    Console.WriteLine ("Ввывод массива:");
+    for (int i = 0; i < array.Length; i++)
+    {
+    Console.Write (array[i] + "\t");
+    }
+   Console.WriteLine ();
+}
+void RaznichaMinMax (double [] array)
+{
+    double max = array[0];
+    double min = array[0];
+    
+
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] > max) max = array [i];
+        else if (array[i] < min) min = array [i];
+    }
+    double Raznicha = max - min;
+Console.WriteLine ();
+Console.WriteLine ($"Максимальное значение = {max}");
+Console.WriteLine ($"Максимальное значение = {min}");
+Console.WriteLine ($"Разница между максимальным и минимальным элементов массива  = {Raznicha}");
+Console.WriteLine ();
+}
+
+
+FillArray (array);
+PrintArray (array);
+RaznichaMinMax (array);
+
